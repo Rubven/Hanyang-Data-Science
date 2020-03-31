0829b775ab811ea8d8b2f8e6e7ad474f880b0d26
VENTURA, RUBEN <rubenventuras@gmail.com>
2020-03-31 19:51:26 +0900

    MapReduce algorithm used to count the items
    TODO: format the data correctly, write output file, identify FPs

--- project_apriori/apriori.py
+++ project_apriori/apriori.py
@@ -1,33 +1,101 @@
-import multiprocessing
 import sys
+import re
+import multiprocessing
 import time
 
-#Global Setting
+#SETTINGS
 minimumSupport = 1
-inputFileNumber = 2
-outputFileNumber = 3
+inputFilePosition = 2
+outputFilePosition = 3
 filePath = "../"
 
-#Input (reading files)
-def readFile(pool, mainDic):
-    with open(filePath + sys.argv[inputFileNumber]) as f:
-        print("LEÍDO")
+#MAP    
+def map(items):
 
-#Main
+    itemMap = []
+    for i in range(len(items)):
+        itemMap.append([items[i].lower(),1])
+    itemMap.sort()
+    return itemMap
+
+
+#SHUFFLE
+def shuffle(itemMap):
+    
+    dic = {}
+    for i in range(len(itemMap)):
+        if itemMap[i][0] not in dic:
+            dic[itemMap[i][0]] = []
+        dic[itemMap[i][0]].append(itemMap[i][1])
+    return dic
+    
+
+#REDUCE    
+def reduce(mainDic, dic):
+       
+    for item in dic:
+        if item not in mainDic:
+            mainDic[item] = []
+            mainDic[item].append(sum(dic[item]))
+        else:
+            transaction_items = sum(dic[item])
+            mainDic_items = mainDic[item][0]
+            mainDic[item] = []
+            mainDic[item].append(transaction_items + mainDic_items)
+    return mainDic
+         
+def processTransaction(transaction):
+      
+    #clean text (just in case)
+    transaction = re.sub("\n", '', transaction)
+    
+    #MAPPING
+    items = map(transaction.split("\t"))                    
+    items = shuffle(items)
+    return items           
+
+
+def processFile(pool, stack, mainDic):
+    
+    with open(filePath + sys.argv[inputFilePosition]) as f:            
+        #send threads to handle different transactions
+        for transaction in f: 
+            async_result = pool.apply_async(processTransaction, args = (transaction, ))
+            dic = async_result.get()
+
+            #add result to the stack to handle it when its turn comes
+            stack.append(dic)
+
+     
 def main():
 
-    #Setting thread pool (takes # of cores by default)
+    #Setting thread pool (takes machine's # of cores by default)
     pool = multiprocessing.Pool(1)
-
+    
     #Global dictionay contaning the frequent transactions
     mainDic = {}
+   
+    #stack to collect the solutions from the threads and manage them in order, so that they don't overlap
+    stack = []
 
-    readFile(pool, mainDic)
+    #read the file and identify the items
+    processFile(pool, stack, mainDic)
 
-    print("Test")
+    #manage concurrency      
+    wait = True
+    while wait:
+        if len(stack) > 0:
+            wait = False
+    
+    #modify main dictionary
+    while (len(stack) > 0):
+        mainDic = reduce(mainDic, stack.pop())
 
-#Execute (with time)
+    print("mainDic:\n")      
+    print (mainDic)   
+
+#time counter to show the execution time    
 if __name__ == '__main__':
-    startTime = time.time()
-    main()
-    print("Execution time: %ss" % (time.time() - startTime) )
+   startTime = time.time()
+   main()
+   print("Execution time: %ss" % (time.time() - startTime))
